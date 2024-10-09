9ba07cdfbc24169f7eb969af7e0546e676e668e2
skiller70 <singhlucky007007@gmail.com>
2024-10-09 11:49:27 +0530

    fix amazon host

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-images:{
+  images: {
     remotePatterns: [
-        {
-            protocol: 'https',
-            hostname: 'amplify-amplifytest-singh-amplifyteamdrivebucket28-q9oral1ofnhe.s3.ap-south-1.amazonaws.com',
-            port: '',
-            pathname: '/profile-pictures/**',
-          },
-      ],
-}   
+      {
+        protocol: "https",
+        // hostname: 'amplify-amplifytest-singh-amplifyteamdrivebucket28-q9oral1ofnhe.s3.ap-south-1.amazonaws.com',
+        hostname:
+          "amplify-d2jxt5d9hcjf36-ma-amplifyteamdrivebucket28-n1fsxnyky65o.s3.ap-south-1.amazonaws.com",
+        port: "",
+        pathname: "/profile-pictures/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
